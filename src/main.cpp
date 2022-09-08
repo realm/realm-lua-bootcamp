10d35e3e577387b32441f0038e6dbd15692f66f3
Yavor Georgiev <fealebenpae@gmail.com>
2022-09-08 17:11:39 +0200

    Enable debugging of Lua code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <filesystem>
+#include <string>
 
 #include <realm.h>
 #include <lua.hpp>
@@ -103,7 +103,62 @@ static void realm_first_steps() {
     realm_release(realm);
 }
 
+static int msghandler(lua_State *L) {
+    const char *msg = lua_tostring(L, 1);
+    if (msg == NULL) {  /* is error object not a string? */
+        if (luaL_callmeta(L, 1, "__tostring") &&  /* does it have a metamethod */
+            lua_type(L, -1) == LUA_TSTRING)  /* that produces a string? */
+            return 1;  /* that is the message */
+    else
+        msg = lua_pushfstring(L, "(error object is a %s value)", luaL_typename(L, 1));
+    }
+    luaL_traceback(L, L, msg, 1);  /* append a standard traceback */
+    return 1;  /* return the traceback */
+}
+
+static int report(lua_State *L, int status) {
+    if (status != LUA_OK) {
+        const char *msg = lua_tostring(L, -1);
+        lua_writestringerror("%s\n", msg);
+        lua_pop(L, 1);  /* remove message */
+    }
+    return status;
+}
+
+static int docall(lua_State *L, int narg, int nres) {
+    int status;
+    int base = lua_gettop(L) - narg;  /* function index */
+    lua_pushcfunction(L, msghandler);  /* push message handler */
+    lua_insert(L, base);  /* put it under function and args */
+    status = lua_pcall(L, narg, nres, base);
+    lua_remove(L, base);  /* remove message handler from the stack */
+    return status;
+}
+
+static int dochunk(lua_State *L, int status) {
+    if (status == LUA_OK) status = docall(L, 0, 0);
+    return report(L, status);
+}
+
+
+static int dofile(lua_State *L, const char *name) {
+    return dochunk(L, luaL_loadfile(L, name));
+}
+
+
+static int dostring(lua_State *L, const char *s, const char *name) {
+    return dochunk(L, luaL_loadbuffer(L, s, strlen(s), name));
+}
+
 int main(int argc, char** argv) {
+    std::string lua_path;
+    if (const char* existing_path = getenv("LUA_PATH")) {
+        lua_path = existing_path;
+        lua_path += ";";
+    }
+    lua_path += SCRIPT_SOURCE_PATH "/lib/?/init.lua;;";
+    setenv("LUA_PATH", lua_path.c_str(), true);
+
     // Create a new instance of the Lua VM state object
     lua_State* L = luaL_newstate();
 
@@ -111,31 +166,26 @@ int main(int argc, char** argv) {
     luaL_openlibs(L);
     realm_lib_open(L);
 
-    int status;
+    const char* file = SCRIPT_SOURCE_PATH"/main.lua";
 
-    status = luaL_dostring(L, "package.path = package.path .. ';" SCRIPT_SOURCE_PATH "/lib/?/init.lua'");
-    if (status) {
-        std::cerr << "Error setting up package search path: " << lua_tostring(L, -1) << std::endl;
-        return 1;
+    if (argc > 1) {
+        // arguments here are what the Lua vscode debugger passes to inject itself
+        for (int i = 1; i < argc; i++) {
+            if (argv[i][0] == '-') {
+                switch (argv[i][1]) {
+                    case 'e':
+                        dostring(L, argv[++i], "=(command line)");
+                        break;
+                    default:
+                        assert(false);
+                }
+            } else {
+                file = argv[i];
+            }
+        }
     }
 
-    // Load script file and push it onto the stack
-    status = luaL_loadfile(L, SCRIPT_SOURCE_PATH"/main.lua");
-    if (status) {
-        std::cerr << "Error loading file: " << lua_tostring(L, -1) << std::endl;
-        return 1;
-    }
-
-    // Execute the value at top of the stack
-    status = lua_pcall(L, 0, LUA_MULTRET, 0);
-    if (status) {
-        std::cerr << "Error running script: " << lua_tostring(L, -1) << std::endl;
-        return 1;
-    }
-
+    dofile(L, file);
     lua_close(L);
-
-    realm_first_steps();
-
     return 0;
 }

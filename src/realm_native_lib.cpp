f9bc8897793898423d86549d464d23b57867d2d1
LJ <81748770+elle-j@users.noreply.github.com>
2022-09-21 08:49:43 +0200

    Add native functions associated with realm app.

--- src/realm_native_lib.cpp
+++ src/realm_native_lib.cpp
@@ -9,6 +9,83 @@
 #include "realm_schema.hpp"
 #include "realm_util.hpp"
 
+static int _lib_realm_app_create(lua_State* L) {
+
+    // realm.h:
+
+    // Create a realm_app_config_t
+    // RLM_API realm_app_config_t* realm_app_config_new(const char* app_id,
+    //                                              const realm_http_transport_t* http_transport) RLM_API_NOEXCEPT;
+
+    // Possibly set more fields on the config object (if also passed from Lua)
+    // RLM_API void realm_app_config_set_base_url(realm_app_config_t*, const char*) RLM_API_NOEXCEPT;
+    // RLM_API void realm_app_config_set_local_app_name(realm_app_config_t*, const char*) RLM_API_NOEXCEPT;
+    // RLM_API void realm_app_config_set_local_app_version(realm_app_config_t*, const char*) RLM_API_NOEXCEPT;
+
+    // Create a realm_app_t
+    // RLM_API realm_app_t* realm_app_create(const realm_app_config_t*, const realm_sync_client_config_t*);
+
+    return 1;
+}
+
+static int _lib_realm_app_register_email(lua_State* L) {
+
+    // realm.h
+
+    // RLM_API bool realm_app_email_password_provider_client_register_email(realm_app_t* app, const char* email,
+    //                                                                  realm_string_t password,
+    //                                                                  realm_app_void_completion_func_t callback,
+    //                                                                  realm_userdata_t userdata,
+    //                                                                  realm_free_userdata_func_t userdata_free);
+
+
+    return 1;
+}
+
+static int _lib_realm_app_credentials_new_email_password(lua_State* L) {
+
+    // realm.h
+
+    // RLM_API realm_app_credentials_t* realm_app_credentials_new_email_password(const char* email,
+    //                                                                       realm_string_t password) RLM_API_NOEXCEPT;
+
+    return 1;
+}
+
+static int _lib_realm_app_log_in(lua_State* L) {
+
+    // realm.h
+
+    // RLM_API bool realm_app_log_in_with_credentials(realm_app_t* app, realm_app_credentials_t* credentials,
+    //                                            realm_app_user_completion_func_t callback, realm_userdata_t userdata,
+    //                                            realm_free_userdata_func_t userdata_free);
+
+    return 1;
+}
+
+static int _lib_realm_user_log_out(lua_State* L) {
+
+    // realm.h
+
+    // RLM_API bool realm_user_log_out(realm_user_t*);
+
+
+    // NOTE: There is also:
+    // RLM_API bool realm_app_log_out(realm_app_t* app, realm_user_t* user, realm_app_void_completion_func_t callback,
+    //                            realm_userdata_t userdata, realm_free_userdata_func_t userdata_free);
+
+    return 1;
+}
+
+static int _lib_realm_user_get_id(lua_State* L) {
+
+    // realm.h
+
+    // RLM_API const char* realm_user_get_identity(const realm_user_t* user) RLM_API_NOEXCEPT;
+
+    return 1;
+}
+
 static int _lib_realm_open(lua_State* L) {
     luaL_checktype(L, 1, LUA_TTABLE);
 
@@ -323,20 +400,26 @@ static int _lib_realm_results_filter(lua_State *L){
 }
 
 static const luaL_Reg lib[] = {
-  {"realm_open",                    _lib_realm_open},
-  {"realm_release",                 _lib_realm_release},
-  {"realm_begin_write",             _lib_realm_begin_write},
-  {"realm_commit_transaction",      _lib_realm_commit_transaction},
-  {"realm_cancel_transaction",      _lib_realm_cancel_transaction},
-  {"realm_object_create",           _lib_realm_object_create},
-  {"realm_set_value",               _lib_realm_set_value},
-  {"realm_get_value",               _lib_realm_get_value},
-  {"realm_object_get_all",          _lib_realm_object_get_all},
-  {"realm_object_add_listener",     _lib_realm_object_add_listener},
-  {"realm_results_get",             _lib_realm_results_get},
-  {"realm_results_count",           _lib_realm_results_count},
-  {"realm_results_add_listener",    _lib_realm_results_add_listener},
-  {"realm_results_filter",          _lib_realm_results_filter},
+  {"realm_app_create",                          _lib_realm_app_create},
+  {"realm_app_register_email",                  _lib_realm_app_register_email},
+  {"realm_app_credentials_new_email_password",  _lib_realm_app_credentials_new_email_password},
+  {"realm_app_log_in",                          _lib_realm_app_log_in},
+  {"realm_user_log_out",                        _lib_realm_user_log_out},
+  {"realm_user_get_id",                         _lib_realm_user_get_id},
+  {"realm_open",                                _lib_realm_open},
+  {"realm_release",                             _lib_realm_release},
+  {"realm_begin_write",                         _lib_realm_begin_write},
+  {"realm_commit_transaction",                  _lib_realm_commit_transaction},
+  {"realm_cancel_transaction",                  _lib_realm_cancel_transaction},
+  {"realm_object_create",                       _lib_realm_object_create},
+  {"realm_set_value",                           _lib_realm_set_value},
+  {"realm_get_value",                           _lib_realm_get_value},
+  {"realm_object_get_all",                      _lib_realm_object_get_all},
+  {"realm_object_add_listener",                 _lib_realm_object_add_listener},
+  {"realm_results_get",                         _lib_realm_results_get},
+  {"realm_results_count",                       _lib_realm_results_count},
+  {"realm_results_add_listener",                _lib_realm_results_add_listener},
+  {"realm_results_filter",                      _lib_realm_results_filter},
   {NULL, NULL}
 };
 

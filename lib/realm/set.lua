51ea533935319387fad374de6b0da4ae8fff7366
Jacob Gunnarsson <jacob.gunnarsson@mongodb.com>
2022-12-30 17:43:36 +0100

    fix set bug

--- lib/realm/set.lua
+++ lib/realm/set.lua
@@ -20,7 +20,7 @@ function RealmSet:new(realm, handle, classInfo)
     return setmetatable(set, RealmSet)
 end
 
---- @param value any The value to lookup in the set.
+--- @param value any The value to look up in the set.
 function RealmSet:__index(value)
     if type(value) == "table" then
         value = value._handle
@@ -30,6 +30,9 @@ end
 
 --- @param value true | nil True implies that the entry should be inserted in the set. Nil means deletion.
 function RealmSet:__newindex(entry, value)
+    if type(entry) == "table" then
+        entry = entry._handle
+    end
     if value == nil then
         native.realm_set_erase(self._handle, entry)
         return
@@ -37,9 +40,6 @@ function RealmSet:__newindex(entry, value)
     if not value == true then
         error("Entries in a set can only have a value of true")
         return
-    end
-    if type(entry) == "table" then
-        entry = entry._handle
     end
     native.realm_set_insert(self._handle, entry)
 end
